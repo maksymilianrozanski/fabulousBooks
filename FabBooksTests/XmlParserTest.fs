6a4105d7cfb939b803410132927bd2c8afb402d9
Max <maksymilianrozanski@gmail.com>
2020-06-01 14:09:00 +0200

    test more xml values

--- FabBooksTests/XmlParserTest.fs
+++ FabBooksTests/XmlParserTest.fs
@@ -9,9 +9,26 @@ let shouldPass () = Assert.AreEqual(2, 2)
 [<Test>]
 let ShouldParseXmlTest () =
 
-    let totalResults = 752
-    
-    let input = sprintf """<?xml version="1.0" encoding="UTF-8"?>
+    let resultsStart = 10
+    let resultsEnd = 20
+    let totalResults = 1222
+
+    let originalPublicationYear1 = 1999
+    let averageRating1 = 4.20
+    let title1 = "Harry Potter"
+    let author1 = "JK Rowling"
+    let imageUrl1 = "https://example.com"
+    let smallImageUrl1 = "https://example2.com"
+
+    let originalPublicationYear2 = 2000
+    let averageRating2 = 4.30
+    let title2 = "Harry Potter2"
+    let author2 = "JK Rowling"
+    let imageUrl2 = "https://example3.com"
+    let smallImageUrl2 = "https://example4.com"
+
+    let input =
+        sprintf """<?xml version="1.0" encoding="UTF-8"?>
 <GoodreadsResponse>
     <Request>
         <authentication>true</authentication>
@@ -20,8 +37,8 @@ let ShouldParseXmlTest () =
     </Request>
     <search>
         <query><![CDATA[Ender's Game]]></query>
-        <results-start>1</results-start>
-        <results-end>20</results-end>
+        <results-start>%i</results-start>
+        <results-end>%i</results-end>
         <total-results>%i</total-results>
         <source>Goodreads</source>
         <query-time-seconds>0.06</query-time-seconds>
@@ -31,22 +48,22 @@ let ShouldParseXmlTest () =
                 <books_count type="integer">254</books_count>
                 <ratings_count type="integer">1091580</ratings_count>
                 <text_reviews_count type="integer">42631</text_reviews_count>
-                <original_publication_year type="integer">1985</original_publication_year>
+                <original_publication_year type="integer">%i</original_publication_year>
                 <original_publication_month type="integer" nil="true" />
                 <original_publication_day type="integer" nil="true" />
-                <average_rating>4.30</average_rating>
+                <average_rating>%f</average_rating>
                 <best_book type="Book">
                     <id type="integer">375802</id>
-                    <title>Ender’s Game (Ender’s Saga, #1)</title>
+                    <title>%s</title>
                     <author>
                         <id type="integer">589</id>
-                        <name>Orson Scott Card</name>
+                        <name>%s</name>
                     </author>
                     <image_url>
-                        https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1408303130l/375802._SY160_.jpg
+                        %s
                     </image_url>
                     <small_image_url>
-                        https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1408303130l/375802._SY75_.jpg
+                        %s
                     </small_image_url>
                 </best_book>
             </work>
@@ -56,22 +73,22 @@ let ShouldParseXmlTest () =
                 <books_count type="integer">65</books_count>
                 <ratings_count type="integer">83212</ratings_count>
                 <text_reviews_count type="integer">884</text_reviews_count>
-                <original_publication_year type="integer">1984</original_publication_year>
+                <original_publication_year type="integer">%i</original_publication_year>
                 <original_publication_month type="integer">12</original_publication_month>
                 <original_publication_day type="integer" nil="true" />
-                <average_rating>4.18</average_rating>
+                <average_rating>%f</average_rating>
                 <best_book type="Book">
                     <id type="integer">44687</id>
-                    <title>Enchanters' End Game (The Belgariad, #5)</title>
+                    <title>%s</title>
                     <author>
                         <id type="integer">8732</id>
-                        <name>David Eddings</name>
+                        <name>%s</name>
                     </author>
                     <image_url>
-                        https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1217735909l/44687._SX98_.jpg
+                        %s
                     </image_url>
                     <small_image_url>
-                        https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1217735909l/44687._SY75_.jpg
+                        %s
                     </small_image_url>
                 </best_book>
             </work>
@@ -79,7 +96,25 @@ let ShouldParseXmlTest () =
         </results>
     </search>
 
-</GoodreadsResponse>""" totalResults
+</GoodreadsResponse>""" resultsStart resultsEnd totalResults originalPublicationYear1 averageRating1 title1 author1
+            imageUrl1 smallImageUrl1 originalPublicationYear2 averageRating2 title2 author2 imageUrl2 smallImageUrl2
 
     let result = GoodreadsResponse.Parse(input)
+    Assert.AreEqual(resultsStart, result.Search.ResultsStart)
+    Assert.AreEqual(resultsEnd, result.Search.ResultsEnd)
     Assert.AreEqual(totalResults, result.Search.TotalResults)
+    
+    Assert.AreEqual(originalPublicationYear1, result.Search.Results.Works.[0].OriginalPublicationYear.Value.Value)
+    Assert.AreEqual(averageRating1, result.Search.Results.Works.[0].AverageRating)
+    Assert.AreEqual(title1, result.Search.Results.Works.[0].BestBook.Title)
+    Assert.AreEqual(author1, result.Search.Results.Works.[0].BestBook.Author.Name)
+    Assert.AreEqual(imageUrl1, result.Search.Results.Works.[0].BestBook.ImageUrl.Trim().TrimEnd())
+    Assert.AreEqual(smallImageUrl1, result.Search.Results.Works.[0].BestBook.SmallImageUrl.Trim().TrimEnd())
+    
+    Assert.AreEqual(originalPublicationYear2, result.Search.Results.Works.[1].OriginalPublicationYear.Value.Value)
+    Assert.AreEqual(averageRating2, result.Search.Results.Works.[1].AverageRating)
+    Assert.AreEqual(title2, result.Search.Results.Works.[1].BestBook.Title)
+    Assert.AreEqual(author2, result.Search.Results.Works.[1].BestBook.Author.Name)
+    Assert.AreEqual(imageUrl2, result.Search.Results.Works.[1].BestBook.ImageUrl.Trim().TrimEnd())
+    Assert.AreEqual(smallImageUrl2, result.Search.Results.Works.[1].BestBook.SmallImageUrl.Trim().TrimEnd())
+    
