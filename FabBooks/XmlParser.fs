cf10ebbd2a56a45278d2a867251bc8d22f27e3ce
Max <maksymilianrozanski@gmail.com>
2020-06-05 16:11:33 +0200

    add embedded resource value in XmlProvider

--- FabBooks/XmlParser.fs
+++ FabBooks/XmlParser.fs
@@ -3,6 +3,7 @@ namespace FabBooks
 open FSharp.Data
 
 module XmlParser =
-    type GoodreadsSearchResponse = XmlProvider<"searchresponse.xml">
+    type GoodreadsSearchResponse = XmlProvider<"./searchresponse.xml", EmbeddedResource="FabBooks, searchresponse.xml">
 
-    type GoodreadsBookResponse = XmlProvider<"bookdetailsresponse.xml">
+    type GoodreadsBookResponse =
+        XmlProvider<"./bookdetailsresponse.xml", EmbeddedResource="FabBooks, bookdetailsresponse.xml">
