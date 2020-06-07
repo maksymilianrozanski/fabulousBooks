e97a1964641ba5912e685f86ee1ac973e2885495
Max <maksymilianrozanski@gmail.com>
2020-06-07 10:17:58 +0200

    add shouldUpdateResponseModelAndStatus test

--- FabBooksTests/UpdateTests.fs
+++ FabBooksTests/UpdateTests.fs
@@ -1,17 +1,20 @@
 module FabBooksTests.UpdateTests
 
 open System
+open System.Collections.Generic
 open System.Web
 open FabBooks
 open FabBooks.BookDetailsPage
+open FabBooks.BookItemModule
 open FabBooks.MainMessages
 open NUnit.Framework
 open App
 open GoodreadsResponseModelModule
+open NUnit.Framework
 
 [<Test>]
 let shouldUpdateTextAndStatus () =
-    let initModel =
+    let initialModel =
         { EnteredText = "Init text"
           Status = Status.Success
           ResponseModel = emptyGoodreadsModel
@@ -25,5 +28,28 @@ let shouldUpdateTextAndStatus () =
           ResponseModel = emptyGoodreadsModel
           BookDetailsPageModel = None }, [ PerformSearchCmd queryText ]
 
-    let result = App.update (Msg.PerformSearch queryText) (initModel)
+    let result = App.update (Msg.PerformSearch queryText) (initialModel)
+    Assert.AreEqual(expected, result)
+
+[<Test>]
+let shouldUpdateResponseModelAndStatus () =
+    let initialModel =
+        { EnteredText = "Init text"
+          Status = Status.Loading
+          ResponseModel = emptyGoodreadsModel
+          BookDetailsPageModel = None }
+
+    let receivedResponseModel =
+        GoodreadsResponseModel
+            (true, 0, 1, 1,
+             System.Collections.Generic.List
+                 [ BookItem("Author", "Title", "https://example.com", "https://example.com", 42) ])
+
+    let expected =
+        { EnteredText = "Init text"
+          Status = Status.Success
+          ResponseModel = receivedResponseModel
+          BookDetailsPageModel = None }, []
+
+    let result = App.update (Msg.SearchResultReceived receivedResponseModel) initialModel
     Assert.AreEqual(expected, result)
