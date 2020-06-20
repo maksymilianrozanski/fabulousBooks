72ddee4604c00f263ef79c9f7cc3bef2660af4b0
Max <maksymilianrozanski@gmail.com>
2020-06-20 16:56:30 +0200

    add PreferencesModule.fs

---
+++ StorageLib/PreferencesModule.fs
@@ -0,0 +1,17 @@
+namespace FabBooks
+
+module PreferencesModule =
+
+    open Xamarin.Essentials
+
+    let private apiKey = "preferencesGoodreadsApiKey"
+
+    let saveApiKey (key: string) =
+        Preferences.Set(apiKey, key)
+
+    let getApiKey =
+        match Preferences.Get(apiKey, "") with
+        | "" -> Option.None
+        | x -> Some(x)
+
+    let deleteKey = Preferences.Remove(apiKey)
